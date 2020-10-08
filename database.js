5d92c38c4a567c174ef6f66405e383d5d5b4fc67
Ghabriel Schiffelbein Fiorotti <ghabriel@ucl.br>
2020-10-07 23:33:45 -0300

    Mudando conexão

--- database.js
+++ database.js
@@ -20,7 +20,7 @@ module.exports = {
 
 const Sequelize = require('sequelize');
 require('dotenv/config');
-const sequelize = new Sequelize("postgres://oegfitrzdodecg:46739d82d1c2494896c7c730565886e0765d543bc8e13571626de1ad90650735@ec2-52-73-199-211.compute-1.amazonaws.com:5432/d815f7vv6rutqf?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory")
+const sequelize = new Sequelize("postgres://oegfitrzdodecg:46739d82d1c2494896c7c730565886e0765d543bc8e13571626de1ad90650735@ec2-52-73-199-211.compute-1.amazonaws.com:5432/d815f7vv6rutqf")
 //postgres://${process.env.USER}:${process.env.PWD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}
 console.log("passou")
 
