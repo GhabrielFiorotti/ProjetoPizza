6680656c2c07af9a8ce00f1eaa6ad078fb23a41a
Ghabriel Schiffelbein Fiorotti <ghabriel@ucl.br>
2020-10-07 23:42:54 -0300

    Mudando a conexão

--- model/Pizza.js
+++ model/Pizza.js
@@ -3,8 +3,8 @@ const database = require('../database');
 
 const pizzas = database.sequelize.define('Pizza', {
     pizza_id : {type: database.Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-    nome : {type : database.Sequelize.STRING(50), allowNull : false},
-    tamanho : {type : database.Sequelize.STRING(1), allowNull : false},
+    nome : {type : database.Sequelize.STRING(255), allowNull : false},
+    tamanho : {type : database.Sequelize.STRING(255), allowNull : false},
     valor : {type : database.Sequelize.STRING(5),allowNull : false},
     img : {type : database.Sequelize.STRING(50)}
     
