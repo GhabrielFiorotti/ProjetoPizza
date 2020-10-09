d7f43e2785790bc4cfcb85d9efb7cccd6dbb57f5
GhabrielFiorotti <50412408+GhabrielFiorotti@users.noreply.github.com>
2020-10-09 11:22:01 -0300

    Update pizzas.js

--- pizzas.js
+++ pizzas.js
@@ -37,10 +37,6 @@ router.post('/cadastroPizza', async (req, res) => {
     }
 );
 
-router.get('/imgpizza', function(req, res){
-  res.sendFile(path.join(__dirname+'/img/pizza1.jpg'));
-});
-
 /* 
 
 router.post('/login', async (req, res) => {
