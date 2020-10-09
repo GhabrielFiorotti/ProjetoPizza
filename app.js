45548169586fff3ee62ceaeea7df3cd443649b10
GhabrielFiorotti <50412408+GhabrielFiorotti@users.noreply.github.com>
2020-10-09 15:20:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors())
+app.use(express.static('public'))
 app.use(express.json());
 
 const rotaPizza = require('./pizzas');
